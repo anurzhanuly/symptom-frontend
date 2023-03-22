15ce5361dfd95fab9e2d96a04b0364c1e5d2efb0
Almuker <73536819+Almuker@users.noreply.github.com>
2023-03-22 21:30:59 +0600

    ignore dist

--- dist/assets/PatientCabinetSettings.71e5c17f.js
+++
@@ -1,1 +0,0 @@
-import{a2 as e,a as t,b as n}from"./index.10d243cd.js";const a={};function c(r,s){return t(),n("h1",null,"Settings")}const _=e(a,[["render",c]]);export{_ as default};
--- dist/assets/overlayeventbus.esm.217c50b1.js
+++
@@ -1,1 +0,0 @@
-import{V as r}from"./index.10d243cd.js";var e=r();export{e as O};
