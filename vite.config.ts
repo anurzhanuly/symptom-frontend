bcc6cc8439634434f5e96e3afb89cf6aeb5c1c6f
Islamzhon Karimov <karimovi@kolesa.kz>
2023-06-06 23:57:20 +0600

    main feat: компрессия текста

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        compression({
+            ext: '.gz', // compressed file ext
+            deleteOriginFile: false, // delete original files
+            algorithm: 'gzip', // compression type, default is 'gzip'
+            threshold: 10240, // only compress files bigger than this size (in bytes)
+        }),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
