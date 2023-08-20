2c193b50433e129e70d9e7c9017606d1c36ca3cd
Islamzhon Karimov <karimovi@kolesa.kz>
2023-08-20 15:06:52 +0600

    nuxt linter add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    modules: [
-        '@pinia/nuxt',
-        '@nuxtjs/eslint-module',
-    ],
-    pinia: {
-        autoImports: [
-            // automatically imports `defineStore`
-            'defineStore', // import { defineStore } from 'pinia'
-            ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
-        ],
-    },
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/eslint-module'
+  ],
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      'defineStore', // import { defineStore } from 'pinia'
+      ['defineStore', 'definePiniaStore'] // import { defineStore as definePiniaStore } from 'pinia'
+    ]
+  },
+  buildModules: [
+    ['@nuxtjs/eslint-module', {
+      fix: true
+    }]
+  ]
 })
