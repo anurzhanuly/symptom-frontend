14db5832d15be1d3c2352169d7e2404dc8f04dee
Islamzhon Karimov <karimovi@kolesa.kz>
2023-08-20 15:03:52 +0600

    nuxt linter add

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
     <NuxtWelcome />
-  </div>
+    <NuxtWelcome />
 </template>
+
+<script setup lang="ts">
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: { enabled: true },
+    modules: [
+        '@pinia/nuxt',
+        '@nuxtjs/eslint-module',
+    ],
+    pinia: {
+        autoImports: [
+            // automatically imports `defineStore`
+            'defineStore', // import { defineStore } from 'pinia'
+            ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+        ],
+    },
 })
