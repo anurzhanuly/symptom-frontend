1c16c731ed9e5bb5874ad80795f090b773713693
Sarvar <ksz95@bk.ru>
2023-09-14 21:44:46 +0600

    fix:

--- src/mobile/modules/authorization/components/Authorization.vue
+++ src/mobile/modules/authorization/components/Authorization.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import BaseCarousel from '@/components/BaseCarousel.vue';
+import BaseCarousel from '@mobile/components/BaseCarousel.vue';
 import UiButton from '@/ui/UiButton.vue';
 </script>
 
