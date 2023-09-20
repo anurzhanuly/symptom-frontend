95907c54688c43b4dec6ba0efc0b3f36a10db477
Sarvar <ksz95@bk.ru>
2023-09-20 15:08:14 +0600

    refactor: Refactor style desktop home about component

--- src/desktop/modules/home/components/AboutProject.vue
+++ src/desktop/modules/home/components/AboutProject.vue
@@ -1,24 +1,10 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 import { useHomeStore } from '../store/home.store';
-import { useRouter } from 'vue-router';
 import { computed } from 'vue';
-
-import UiButton from '@/ui/UiButton.vue';
 
 const homeStore = useHomeStore();
 const { isDoctor } = storeToRefs(homeStore);
-const router = useRouter();
-
-const actionInfo = computed(() => {
-    return isDoctor.value
-        ? 'Оставьте заявку в Symptom уже сейчас'
-        : 'Определить состояние здоровья уже сейчас';
-});
-
-const buttonLabel = computed(() => {
-    return isDoctor.value ? 'Оставить заявку' : 'Пройти опрос';
-});
 
 const projectInfoFirst = computed(() => {
     return isDoctor.value
@@ -31,141 +17,95 @@ const projectInfoSecond = computed(() => {
         ? 'С помощью опросника, работающего на базе искусственного интеллекта, Symptom собирает структурированную историю болезни'
         : 'Опросник состоит из вопросов, составленных профессиональными врачами. Его заполнение опросника занимает от 5 до 15 минут в зависимости от вашего состояния.';
 });
-
-function direct(): void {
-    if (isDoctor.value) {
-        // TODO registration
-    } else {
-        router.push({ name: 'choose-survey-flow' });
-    }
-}
 </script>
 
 <template>
-    <div
-        id="test"
-        class="about"
-    >
-        <div class="about-container">
-            <div class="image">
-                <img
-                    alt="logo"
-                    src="@/assets/onboarding/onboard2.png"
-                />
+    <div class="about">
+        <img
+            class="about__img"
+            alt="logo"
+            src="@/assets/onboarding/onboard2.png"
+        />
+        <div class="about__info-block">
+            <h3 class="about__info-block__title">О проекте</h3>
+            <div class="about__info-block__content">
+                <h1 class="about__info-block__content__title">01</h1>
+                <p class="about__info-block__content__text">
+                    {{ projectInfoFirst }}
+                </p>
             </div>
-            <div class="info">
-                <h3>О проекте</h3>
-                <div>
-                    <div class="project-info">
-                        <h1>01</h1>
-                        <p>{{ projectInfoFirst }}</p>
-                    </div>
-                    <div class="project-info">
-                        <h1>02</h1>
-                        <p>{{ projectInfoSecond }}</p>
-                    </div>
-                </div>
+            <div class="about__info-block__content">
+                <h1 class="about__info-block__content__title">02</h1>
+                <p class="about__info-block__content__text">
+                    {{ projectInfoSecond }}
+                </p>
             </div>
-        </div>
-    </div>
-    <div class="about-action">
-        <h4>{{ actionInfo }}</h4>
-        <div class="about__button">
-            <ui-button
-                is-big
-                is-full
-                is-blue
-                @click="direct"
-            >
-                Пройти Опрос
-            </ui-button>
         </div>
     </div>
 </template>
 
-<style scoped>
-h4 {
-    font-weight: 600;
-    font-size: 36px;
-    color: #212121;
-}
-
-h3 {
-    font-weight: 600;
-    font-size: 36px;
-    line-height: 44px;
-    color: #111111;
-    margin-left: 75px;
-}
-
-h1 {
-    font-weight: 600;
-    font-size: 60px;
-    line-height: 60px;
-    color: #276ef1;
-    margin-right: 15px;
-}
-
-p {
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 28px;
-    color: #000000;
-}
-
-.info {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 100%;
-}
-
-.project-info {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 30px;
-}
-
+<style scoped lang="scss">
 .about {
-    background: #eef5fb;
-}
-
-.about-container {
     display: flex;
     padding: 20px 100px;
-}
+    background: #eef5fb;
 
-.image,
-img {
-    width: 100%;
-}
+    &__img {
+        max-width: 50%;
+        max-height: 100%;
 
-.about-action {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 22vh;
-    justify-content: space-evenly;
-    background: linear-gradient(135deg, #62a9e3 15.72%, #aacceb 80.75%);
-}
-
-.about__button {
-    width: 55%;
-}
-
-@media (max-width: 1350px) {
-    h1 {
-        font-size: 50px;
+        @media (max-width: 1200px) {
+            display: none;
+        }
     }
 
-    p {
-        font-size: 18px;
-    }
-}
+    &__info-block {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 100%;
 
-@media (max-width: 1200px) {
-    .image {
-        display: none;
+        &__title {
+            font-weight: 600;
+            font-size: 36px;
+            line-height: 44px;
+            color: #111111;
+            margin-left: 75px;
+
+            @media (max-width: 1200px) {
+                margin: 0;
+                text-align: center;
+            }
+        }
+
+        &__content {
+            display: flex;
+            justify-content: space-between;
+            margin-top: 30px;
+
+            &__title {
+                font-weight: 600;
+                font-size: 60px;
+                line-height: 60px;
+                color: #276ef1;
+                margin-right: 15px;
+
+                @media (max-width: 1350px) {
+                    font-size: 50px;
+                }
+            }
+
+            &__text {
+                font-weight: 400;
+                font-size: 20px;
+                line-height: 28px;
+                color: #000000;
+
+                @media (max-width: 1350px) {
+                    font-size: 18px;
+                }
+            }
+        }
     }
 }
 </style>
---
+++ src/desktop/modules/home/components/Action.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useHomeStore } from '../store/home.store';
+import { useRouter } from 'vue-router';
+import { computed } from 'vue';
+
+import UiButton from '@/ui/UiButton.vue';
+
+const homeStore = useHomeStore();
+const { isDoctor } = storeToRefs(homeStore);
+const router = useRouter();
+
+const actionInfo = computed(() => {
+    return isDoctor.value
+        ? 'Оставьте заявку в Symptom уже сейчас'
+        : 'Определить состояние здоровья уже сейчас';
+});
+
+function direct(): void {
+    if (isDoctor.value) {
+        // TODO registration
+    } else {
+        router.push({ name: 'choose-survey-flow' });
+    }
+}
+</script>
+
+<template>
+    <div class="action">
+        <h4 class="action__title">{{ actionInfo }}</h4>
+        <div class="action__button">
+            <ui-button
+                is-big
+                is-full
+                is-blue
+                @click="direct"
+            >
+                Пройти Опрос
+            </ui-button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.action {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 22vh;
+    justify-content: space-evenly;
+    background: linear-gradient(135deg, #62a9e3 15.72%, #aacceb 80.75%);
+
+    &__title {
+        font-weight: 600;
+        font-size: 36px;
+        color: #212121;
+    }
+
+    &__button {
+        width: 50%;
+    }
+}
+</style>
