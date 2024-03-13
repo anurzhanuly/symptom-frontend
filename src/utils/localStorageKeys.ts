8511185238fb018cfdb303f9390637e19f86cbf3
Sarvar <ksz95@bk.ru>
2024-03-13 10:06:14 +0500

    refactor: comments refactor

--- src/utils/localStorageKeys.ts
+++ src/utils/localStorageKeys.ts
@@ -2,13 +2,13 @@ export const DISEASE_ID = 'diseaseId';
 export const DOCTOR_ID = 'doctorId';
 export const PATIENT_ID = 'patientId';
 
-export const ADMIN_TOKEN = 'admToken';
+export const ADMIN_TOKEN = 'adminToken';
 export const DOCTOR_TOKEN = 'doctorToken';
 export const CLIENT_TOKEN = 'clientToken';
-export const DATE_OF_TOKEN = 'tokenDate';
+export const DATE_OF_TOKEN = 'dateOfToken';
 
 export const SURVEY_FLOW = 'surveyFlow';
-export const CABINET_RESULT_ID = 'clientCabinetResultId';
+export const CABINET_RESULT_ID = 'cabinetResultId';
 
 export const SURVEY_RESULT = 'surveyResult';
 export const SURVEY_CURRENT_ANSWERS = 'surveyCurrentAsnwers';
