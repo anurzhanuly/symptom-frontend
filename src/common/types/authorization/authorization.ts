224e2b4969eea3082dfd8ffd324b8430a56e4498
azatnurzhanuly <AzatNurzhanuly@hollandandbarrett.com>
2024-12-04 11:11:01 +0500

    feat: comments out

--- src/common/types/authorization/authorization.ts
+++ src/common/types/authorization/authorization.ts
@@ -1,21 +1,22 @@
-import type {
-    Attributes,
-    Relationships,
-} from '@desktop/modules/admin/types/clinics';
+export {};
+// import type {
+//     Attributes,
+//     Relationships,
+// } from '@desktop/modules/admin/types/clinics';
 
-export interface ClientRegistration {
-    email: string;
-    password: string;
-    c_password: string;
-    type: string;
-    first_name: string;
-    last_name: string;
-    phone: string;
-}
-
-export interface Doctors {
-    type: string;
-    id: string;
-    attributes: Attributes;
-    relationships?: Relationships;
-}
+// export interface ClientRegistration {
+//     email: string;
+//     password: string;
+//     c_password: string;
+//     type: string;
+//     first_name: string;
+//     last_name: string;
+//     phone: string;
+// }
+//
+// export interface Doctors {
+//     type: string;
+//     id: string;
+//     attributes: Attributes;
+//     relationships?: Relationships;
+// }
