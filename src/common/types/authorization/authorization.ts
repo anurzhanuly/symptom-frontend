6f407e004d52b9d0b59bbdda46aaabb066a15a9b
azatnurzhanuly <AzatNurzhanuly@hollandandbarrett.com>
2024-12-04 11:13:32 +0500

    Revert "feat: comments out"
    This reverts commit 224e2b4969eea3082dfd8ffd324b8430a56e4498.

--- src/common/types/authorization/authorization.ts
+++ src/common/types/authorization/authorization.ts
@@ -1,22 +1,21 @@
-export {};
-// import type {
-//     Attributes,
-//     Relationships,
-// } from '@desktop/modules/admin/types/clinics';
+import type {
+    Attributes,
+    Relationships,
+} from '@desktop/modules/admin/types/clinics';
 
-// export interface ClientRegistration {
-//     email: string;
-//     password: string;
-//     c_password: string;
-//     type: string;
-//     first_name: string;
-//     last_name: string;
-//     phone: string;
-// }
-//
-// export interface Doctors {
-//     type: string;
-//     id: string;
-//     attributes: Attributes;
-//     relationships?: Relationships;
-// }
+export interface ClientRegistration {
+    email: string;
+    password: string;
+    c_password: string;
+    type: string;
+    first_name: string;
+    last_name: string;
+    phone: string;
+}
+
+export interface Doctors {
+    type: string;
+    id: string;
+    attributes: Attributes;
+    relationships?: Relationships;
+}
